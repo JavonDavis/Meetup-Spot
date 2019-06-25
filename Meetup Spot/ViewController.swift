f558bf0eeb3126dec4badb1dcbe7aab741496b6e
Javon <javondavis@users.noreply.github.com>
2019-06-24 23:17:08 -0500

    Initial Commit

---
+++ Meetup Spot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Meetup Spot
+//
+//  Created by Javon Davis on 6/24/19.
+//  Copyright © 2019 Javon Davis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Meetup SpotTests/Meetup_SpotTests.swift
@@ -0,0 +1,34 @@
+//
+//  Meetup_SpotTests.swift
+//  Meetup SpotTests
+//
+//  Created by Javon Davis on 6/24/19.
+//  Copyright © 2019 Javon Davis. All rights reserved.
+//
+
+import XCTest
+@testable import Meetup_Spot
+
+class Meetup_SpotTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
